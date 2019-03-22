279c610ba496d67e0f86e248430ee6ba339a445c
aefrem <aefrem@seas.upenn.edu>
2019-03-22 16:35:09 -0400

    Create Node.py

---
+++ PythonScripts/Node.py
@@ -0,0 +1,12 @@
+class Node:
+
+    def __init__(self, key, raw_question, shortened_question, reference, answer):
+        self.key = key
+        self.raw_question = raw_question
+        self.shortened_question = shortened_question
+        self.reference = reference
+        self.answer = answer
+    
+
+
+    
