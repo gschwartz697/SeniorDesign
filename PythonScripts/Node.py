a069896f5a6336f0a72df7f53500676af64593ed
aefrem <aefrem@seas.upenn.edu>
2019-03-22 16:36:17 -0400

    Update node.py

--- PythonScripts/Node.py
+++ PythonScripts/Node.py
@@ -1,10 +1,10 @@
 class Node:
 
     def __init__(self, key, raw_question, shortened_question, reference, answer):
-        self.key = key
-        self.raw_question = raw_question
+        self.key = key # Type {question number, follow up}
+        self.raw_question = raw_question 
         self.shortened_question = shortened_question
-        self.reference = reference
+        self.reference = reference # Question number
         self.answer = answer
     
 
